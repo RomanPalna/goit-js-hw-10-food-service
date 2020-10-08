15bc077b29297327c02515c2fb0fb0756786cc57
Roman Palna <roma.palna@gmail.com>
2020-10-08 12:50:52 +0300

    done some

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import menu from './menu.json';
+import foodTemplate from './menu-template.hbs';
+
+//Add theme switcher
+const body = document.querySelector('body');
+const switcher = addEventListener('input', switchDarkTheme);
+
+body.classList.toggle('light-theme');
+
+function switchDarkTheme() {
+  if (body.classList.contains('light-theme')) {
+    body.classList.remove('light-theme');
+    body.classList.add('dark-theme');
+  } else {
+    body.classList.remove('dark-theme');
+    body.classList.add('light-theme');
+  }
+}
+
+console.log(body.classList.value);
+
+//Work in progress...
+// localStorage.setItem('theme', );
+
+//Add food marup in html
+const menuMarkup = document.querySelector('.js-menu');
+
+const markup = foodTemplate(menu);
+
+menuMarkup.insertAdjacentHTML('beforeend', markup);
